ff79a6c621046f9f18749af8572a62fb715e153f
Shiraz Kanga <skanga@adobe.com>
2026-09-02 18:23:08 -0700

    Open .md files passed by the OS or command line

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,15 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
-        .run(tauri::generate_context!())
+        .run(
+            tauri::generate_context!(),
+            |app_handle, event| {
+                if let tauri::RunEvent::Opened(paths) = event {
+                    for p in paths {
+                        let _ = app_handle.emit("file-opened", p.to_string_lossy().to_string());
+                    }
+                }
+            },
+        )
         .expect("error while running tauri application");
 }
